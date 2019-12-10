11f425d40c669ff9bd19f8c3fa4bc068d1ee9273
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 23:39:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |describe, array|
+      array.each do |name|
+        if new_hash.has_key?(name)
+          if new_hash[name].has_key?(key)
+            new_hash[name][key] << describe.to_s
+          else
+            new_hash[name][key] = [describe.to_s]
+          end
+
+        else
+          new_hash[name] = {key => [describe.to_s]}
+        end
+      end
+    end
+  end
+  return new_hash
   # write your code here!
 end

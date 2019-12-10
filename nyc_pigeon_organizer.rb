932e1d8556024d935aee18cc9a23fb38e466fd21
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 22:55:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
   # write your code here!
 end

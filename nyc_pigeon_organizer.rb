6538ac0a46712b28965b78aba4ddef42ca7ebdd2
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 23:24:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash| # property: color, gender, lives. #hash: hash
     hash.each do |attribute, array| #attribute: purple, male etc. etc....
       array.each do |name| #name: idx inside array.
+        binding.pry
         if new_hash.has_key?(name) #does hash have name yet?
 
           if new_hash[name].has_key?(property) # if so, does name have name properties? (color/gender/lives)

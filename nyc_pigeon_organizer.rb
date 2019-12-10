b04b9845192c51ea716fc259dca7553b7694a7d2
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 23:20:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,19 +3,18 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
 
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
-        end
-
-        if !new_name[name].has_key?(property)
-          new_hash[name][property] = []
-        end
-
-        if !new_name[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
+  data.each do |property, hash| # property: color, gender, lives. #hash: hash
+    hash.each do |attribute, array| #attribute: purple, male etc. etc....
+      array.each do |name| #name: idx inside array.
+        if new_hash.has_key?(name) #does hash have name yet?
+          
+          if new_hash[name].has_key?(property) # if so, does name have name properties? (color/gender/lives)
+            new_hash[name][property] << attribute.to_s 
+          else
+            new_hash[name][property] = [attribute.to_s]
+          end
+        else
+          hash_name[name] = {hash => [describe.to_s]}
         end
       end
     end
